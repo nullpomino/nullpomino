dc3ec396eb60f3d46e0753340a50dc2d297450b7
w.kowaluk <w.kowaluk@60d36265-0632-6e55-3d55-d59d2894b138>
2010-06-30 00:15:28 +0000

    reverting r23, seems i was wrong

--- play_sdl.bat
+++ play_sdl.bat
@@ -1,3 +1,3 @@
 @echo off
-set PATH=%PATH%;%systemroot%\SysWOW64
+set PATH=lib;%PATH%;%systemroot%\SysWOW64
 start javaw -cp bin;NullpoMino.jar;lib\log4j-1.2.15.jar;lib\sdljava.jar -Djava.library.path=lib org.game_host.hebo.nullpomino.gui.sdl.NullpoMinoSDL
