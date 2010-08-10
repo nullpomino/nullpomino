4c16c3e72c14ed7228429b277aef500d519cd97f
w.kowaluk <w.kowaluk@60d36265-0632-6e55-3d55-d59d2894b138>
2010-08-10 11:57:49 +0000

    -fixed broken BagNoSZORandomizer

--- src/net/omegaboshi/nullpomino/game/subsystem/randomizer/BagNoSZORandomizer.java
+++ src/net/omegaboshi/nullpomino/game/subsystem/randomizer/BagNoSZORandomizer.java
@@ -4,7 +4,7 @@ import mu.nu.nullpo.game.component.Piece;
 
 public class BagNoSZORandomizer extends BagRandomizer {
 	
-	boolean firstBag;
+	boolean firstBag = true;
 	
 	public BagNoSZORandomizer() {
 		super();
@@ -12,16 +12,26 @@ public class BagNoSZORandomizer extends BagRandomizer {
 	
 	public BagNoSZORandomizer(boolean[] pieceEnable, long seed) {
 		super(pieceEnable, seed);
-		firstBag = true;
 	}
 
 	public void shuffle() {
-		if (firstBag && pieces != new int[] {Piece.PIECE_O, Piece.PIECE_Z, Piece.PIECE_S}) {
+		if (firstBag && !isPieceSZOOnly()) {
 			do {
 				super.shuffle();
 			} while (bag[0] == Piece.PIECE_O || bag[0] == Piece.PIECE_Z || bag[0] == Piece.PIECE_S);
+			firstBag = false;
 		} else {
 			super.shuffle();
 		}
+	}
+	
+	private boolean isPieceSZOOnly()
+	{
+		for (int i=0; i<pieces.length; i++) {
+			if (pieces[i] != Piece.PIECE_O && pieces[i] != Piece.PIECE_Z && pieces[i] != Piece.PIECE_S)
+				return false;
+		}
+		
+		return true;
 	}
 }
