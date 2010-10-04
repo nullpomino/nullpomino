a994edfb1ec47f281f37909293d932b493055f35
w.kowaluk <w.kowaluk@60d36265-0632-6e55-3d55-d59d2894b138>
2010-10-04 22:47:42 +0000

    * port override for netserver launch scripts
    Former-commit-id: 5b3f466b3f84d21ae59a0b2bb06e1f5f980ec12e

--- netserver.bat
+++ netserver.bat
@@ -1,3 +1,3 @@
 @echo off
 set path=%path%;%systemroot%\SysWOW64
-start java -cp bin;NullpoMino.jar;lib\log4j-1.2.15.jar mu.nu.nullpo.game.net.NetServer
+start java -cp bin;NullpoMino.jar;lib\log4j-1.2.15.jar mu.nu.nullpo.game.net.NetServer %1
