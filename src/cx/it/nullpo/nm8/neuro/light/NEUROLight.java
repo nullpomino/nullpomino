d822344f9c9f946f96a95f30a5da3934dd31fc7d
zircean@gmail.com <zircean@gmail.com@60d36265-0632-6e55-3d55-d59d2894b138>
2011-07-05 04:17:49 +0000

    *Small overlay test (has nothing to do with the actual overlay)...

--- src/cx/it/nullpo/nm8/neuro/light/NEUROLight.java
+++ src/cx/it/nullpo/nm8/neuro/light/NEUROLight.java
@@ -1,12 +1,18 @@
 package cx.it.nullpo.nm8.neuro.light;
 
+import java.io.IOException;
+
+import cx.it.nullpo.nm8.gui.framework.NFFont;
 import cx.it.nullpo.nm8.gui.framework.NFGraphics;
 import cx.it.nullpo.nm8.gui.framework.NFSystem;
 import cx.it.nullpo.nm8.neuro.core.NEUROCore;
 import cx.it.nullpo.nm8.neuro.error.PluginInitializationException;
+import cx.it.nullpo.nm8.neuro.event.DebugEvent;
 import cx.it.nullpo.nm8.neuro.plugin.Nullterm;
 
 public class NEUROLight extends NEUROCore {
+	
+	NFFont font;
 	
 	/**
 	 * Constructs a NEUROLight.
@@ -16,6 +22,13 @@ public class NEUROLight extends NEUROCore {
 		try {
 			new Nullterm().init(this);
 		} catch (PluginInitializationException e) {}
+		
+		try {
+			font = sys.loadFont("data/res/font/font_neuro.ttf",12,false,false);
+		} catch (IOException e) {
+			font = null;
+			dispatchEvent(new DebugEvent(this,DebugEvent.TYPE_WARNING,"Unable to load NEURO font"));
+		}
 	}
 
 	public String getName() {
@@ -24,6 +37,14 @@ public class NEUROLight extends NEUROCore {
 
 	public float getVersion() {
 		return 0.1F;
+	}
+	
+	public void draw(NFGraphics g) {
+		super.draw(g);
+		if (font != null) {
+			g.setFont(font);
+			g.drawString("POWERED BY "+getName().toUpperCase()+" "+getVersion(),5,450);
+		}
 	}
 
 }
