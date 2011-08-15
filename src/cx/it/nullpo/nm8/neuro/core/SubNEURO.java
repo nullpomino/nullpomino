b55515c2d7cb639ef56bf9db23421d9e92389b52
pbomqlu910963@gmail.com <pbomqlu910963@gmail.com@60d36265-0632-6e55-3d55-d59d2894b138>
2011-08-15 09:29:57 +0000

    #I'm back... maybe. -SubNEURO: Removed bad @Override again

--- src/cx/it/nullpo/nm8/neuro/core/SubNEURO.java
+++ src/cx/it/nullpo/nm8/neuro/core/SubNEURO.java
@@ -5,20 +5,19 @@ import cx.it.nullpo.nm8.neuro.plugin.NEUROPlugin;
 
 /**
  * SubNEURO is a type of NEURO which has the event-passing functionality of NEUROBase, but which is also
- * a plugin. SubNEURO should be extended in order to get the full benefit of 
+ * a plugin. SubNEURO should be extended in order to get the full benefit of
  * @author Zircean
  *
  */
 public abstract class SubNEURO extends NEUROBase implements NEUROPlugin {
-	
+
 	/** The parent NEURO. */
 	protected NEURO neuro;
 
-	@Override
 	public void stop() {
 		stopAll();
 	}
-	
+
 	public final void init(NEURO parent) throws PluginInitializationException {
 		neuro = parent;
 		parent.addPlugin(this);
